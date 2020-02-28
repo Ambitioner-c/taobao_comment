d33f71e5f7144518f295bd07a2ddbbe5379aea8f
Ambitioner-c <656359504@qq.com>
2020-02-28 20:40:20 +0800

    获取并解析评价文件

--- python/comment.py
+++ python/comment.py
@@ -3,11 +3,12 @@ import random
 from time import sleep
 
 
+# 下次运行需要主动更改cookie和referer参数，内容直接复制浏览器控制台信息
 Headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'
                          ' AppleWebKit/537.36 (KHTML, like Gecko)'
                          ' Chrome/80.0.3987.106 Safari/537.36',
-           'cookie': 'cna=gSYsFs5GqgcCAXLV/PLN3yGH; hng=CN%7Czh-CN%7CCNY%7C156; lid=ambitioner%E4%B8%B6; enc=L9HQuV5GHNRsGeq9rZ3xafuSSX2F46GZYB4B2JCyacx3KPhqjHSF%2FRo%2Bw0y9Jx6i4DQECoE%2BjkAgHqXEFzKm1A%3D%3D; _m_h5_tk=29d764e2058930bfd11ab05d80a3cb86_1582804356322; _m_h5_tk_enc=9fb28a8f39883bf19bac2ddcb7ac12d0; sgcookie=DGRVAmM8cBxFOvUJDfVh%2B; uc1=cookie14=UoTUOLRwaXjqcg%3D%3D; t=7458db744d83ab1d06ee636f30708330; uc3=lg2=UIHiLt3xD8xYTw%3D%3D&nk2=Any3BqBNXeXE67%2BP&vt3=F8dBxd3yPWVaWUFtXLY%3D&id2=UUGjMVyuhEU30A%3D%3D; tracknick=ambitioner%5Cu4E36; uc4=nk4=0%40AJc9Jh4DCme1Xd6KuxQkwbBbdrKdGxI%3D&id4=0%40U2OU%2FjjOW%2Bci6bsS%2BHdj1aRVz0Ys; lgc=ambitioner%5Cu4E36; _tb_token_=7e7e5b3b03377; cookie2=145734a209d5cb9239724be258b59208; x5sec=7b22726174656d616e616765723b32223a226266393232346466353634623961343735323239356464663533346366383730434b574c332f4946454a4c796e4d624b2f367a3357673d3d227d; l=dBIU44EgqnlwebYUBOfwZDQk4QQTWdAXCsPr98cToIB1O56Z7dWpxHwEaNbBT3QQEt5AYeKzK6zxJRUp78ULyx_ceTwhKXIpBUJB8e1..; isg=BGRkyc3t5o_MVRGxU2Qu_OUoNWtW_YhnaPBDsH6JiyplKQnzpw7b9yjP7YEx8cC_',
-           'referer': 'https://detail.tmall.com/item.htm?id=602858459541&ali_refid=a3_430583_1006:1103534738:N:FcbnhkUVpVThyuKFufQFZg==:2de1d8285a37a32169146d902490766b&ali_trackid=1_2de1d8285a37a32169146d902490766b&spm=a230r.1.14.1&sku_properties=10004:709990523;5919063:6536025'}
+           'cookie': 'cna=gSYsFs5GqgcCAXLV/PLN3yGH; hng=CN%7Czh-CN%7CCNY%7C156; lid=ambitioner%E4%B8%B6; enc=L9HQuV5GHNRsGeq9rZ3xafuSSX2F46GZYB4B2JCyacx3KPhqjHSF%2FRo%2Bw0y9Jx6i4DQECoE%2BjkAgHqXEFzKm1A%3D%3D; _m_h5_tk=29d764e2058930bfd11ab05d80a3cb86_1582804356322; _m_h5_tk_enc=9fb28a8f39883bf19bac2ddcb7ac12d0; sgcookie=DGRVAmM8cBxFOvUJDfVh%2B; uc1=cookie14=UoTUOLtV3hIT0Q%3D%3D; uc3=lg2=UIHiLt3xD8xYTw%3D%3D&nk2=Any3BqBNXeXE67%2BP&vt3=F8dBxd3yPWVaWUFtXLY%3D&id2=UUGjMVyuhEU30A%3D%3D; t=305a8d13ab3531a86d28e9bb0cb44aa5; tracknick=ambitioner%5Cu4E36; uc4=nk4=0%40AJc9Jh4DCme1Xd6KuxQkwbBbdrKdGxI%3D&id4=0%40U2OU%2FjjOW%2Bci6bsS%2BHdj1aRVz0Ys; lgc=ambitioner%5Cu4E36; _tb_token_=53fe3566ebe74; cookie2=120014477303a6b4aa6e519bb3cec0e9; l=dBIU44EgqnlweYHXBOCZlurza77TlIRfguPzaNbMi_5Ky_L0ShbOoRwjJEp6cjWcM6Tp4JfUk2eT7FZu8z8foTB7K9cdvdnBBef..; isg=BExMFmfIfsaqqGnJK-yGBA0QHaN-hfAv0Phb6KYNNPeMMe07z5fFvsPH1TkJeSiH',
+           'referer': 'https://detail.tmall.com/item.htm?spm=a230r.1.14.13.36cd5022lfyaGb&id=605258110430&cm_id=140105335569ed55e27b&abbucket=10&sku_properties=10004:709990523;5919063:6536025'}
 
 
 def get_comment(page):
@@ -24,9 +25,9 @@ def get_comment(page):
           '&posi=' \
           '&picture=' \
           '&groupId=' \
-          '&ua=098%23E1hvEpvxv7OvUvCkvvvvvjiPn2q9sjE2PLMUtj3mPmPpljnhPLsOQjYnn2SWAjlHRphvCvvvvvmCvpvZz2saf4dNznQGDFnfYMdwFYAC7IVrvpvEvvFQvT0rvPFrdphvmpvWvUnMNQvIsu6Cvvyv2mh2HBZvUnJ5vpvhvvmv9FyCvvpvvvvv2QhvCvvvMMGEvpCWvVtSvvwTD7zOaXZBWD0U%2BExrVTtYVVzOa4QBnk19kbmxfBKKNB3r0j7%2BhfUeCbmDYE7rV16kARLUTE9XaDdXS47BhC3qVUcnDOvfjfyCvm9vvvvnphvvvvvv9krvpvkCvvmm86Cv2vvvvUUdphvUUQvv9krvpv9FkphvC99vvpHgo8yCvv9vvUvgAHkORphCvvOvCvvvphmjvpvhvvpvv86CvCh92U0mNc6vaCOidAI3pQeHA46CvvyvCPKmiTwvNCVrvpvEvvFl9sWsvUAEdphvmpvWgQnI4vmGH46CvvyvCb821wZvpBArvpvEvvkC96lnvh77dphvmpvhoUWcHQmudv%3D%3D&needFold=0' \
-          '&_ksTS=1582839364887_2210' \
-          '&callback=jsonp2211' \
+          '&ua=098%23E1hvcpvPvBhvUpCkvvvvvjiPn2q9tj38PFLZljrCPmPpzjlWRFLO0j3URFcwsj3CPQGCvvpvvPMMvphvC9mvphvvvvyCvhACIzt%2FjaQaRfUTnZJt9b8rVTtYVVzOdiZBRknbkbm65i9Xwhb9%2B2Kzr2E9ZRAn%2BbeAhj3mAXZTKFyzOvxr08TJ%2Bulz8e0xdBuKdeQEKphv8vvvpHIvvvvvvvCHhQvvvLvvvhZLvvmCvvvvBBWvvvHyvvCHhQvvv7oEvpvVvpCmpR2vuphvmvvvpLhKEzV2RphvCvvvphm5vpvhvvCCBUwCvvpv9hCvdphvhppCtKRavvvHNAZNog7EUfvtCQhvCli4zYMwNwGtvpvhvvCvp86CvvyvvZ3n1pvvmCJCvpvZ7DQvvRdw7Di44AM5MCxfsldoz0vtvpvhvvCvpv%3D%3D&needFold=0' \
+          '&_ksTS=1582893081626_352' \
+          '&callback=jsonp353' \
 
     html = requests.get(url, headers=Headers)
     html = html.text
@@ -42,7 +43,7 @@ def doc_write(page, html):
 
 
 if __name__ == '__main__':
-    my_page = 15
+    my_page = 1
     while True:
         sleep(random.randint(2, 6))
 
--- python/read_comment_file.py
+++ python/read_comment_file.py
@@ -5,17 +5,23 @@ import re
 def read(pathname):
     with open(pathname, 'r') as f:
         html = f.readlines()[1]
-        html_dict = json.loads(re.findall(r'jsonp\d+\((.+?)\)', html)[0])
+        html_dict = json.loads(re.findall(r'jsonp\d+\((.+?})\)', html)[0])
 
     return html_dict
 
 
 def analysis(html_dict):
     # 评价
-    print(html_dict['rateDetail']['rateList'][0]['rateContent'])
+    for j in html_dict['rateDetail']['rateList']:
+        print(j['rateContent'])
 
 
 if __name__ == '__main__':
-    my_pathname = '../data/1.html'
+    my_page = 3
+    my_pathname = '../data/' + str(my_page) + '.html'
+
+    # 获取html_dict
     my_html_dict = read(my_pathname)
+
+    # 解析html_dict
     analysis(my_html_dict)
